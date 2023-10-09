90eef2d53e0d90fe3034e4c83de7cf6553da78d0
Techathon <106890862+techathonian@users.noreply.github.com>
2023-10-09 06:34:17 -0700

    Update data.jsx

--- src/store/data.jsx
+++ src/store/data.jsx
@@ -8,7 +8,7 @@ export const NonCodingTrackIcon = ['https://firebasestorage.googleapis.com/v0/b/
 export const Events = [
     {
         eventTitle: 'Future events',
-        text:'Techathon 3.0 is expected to kickstart in the nearest future with lots of opportunities for you to explore, network and learn.'
+        text:'Techathon 4.0 is expected to kickstart in the nearest future with lots of opportunities for you to explore, network and learn.'
 
     }, 
     {
